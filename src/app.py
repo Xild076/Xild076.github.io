728d9dd5dff82977f1650f04639aa447e8d35b15
Harry <harry.d.yin.gpc@gmail.com>
2025-05-02 23:44:17 -0700

    new

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from py2web import Site, Page, Heading, Text, Slider, Button, BrythonSimulation, SiteLink, Container
+from py2web import Site, Page, Heading, Text, Slider, Button, BrythonSimulation, SiteLink
 
 site = Site("EvolutionDemo")
 
@@ -6,11 +6,6 @@ home = Page("index", "Evolution ML Demo", theme="dark")
 home.add(Heading("Evolutionary Simulation", level=1, align="center"))
 home.add(Text("Watch organisms evolve toward the center line.", align="center"))
 home.add(BrythonSimulation("evoCanvas", "evolution_sim.py", "100%", "300px"))
-home.add(Container([
-    Slider("Speed", 1, 10, 1, 5),
-    Slider("Population", 10, 200, 10, 60),
-    Button("Restart", onclick="animate")
-], layout="row", gap="2rem"))
 home.add(SiteLink("about.html", "Learn More"))
 site.add_page(home)
 
